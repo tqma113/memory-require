f5cc55973b84e54d5df1c6bf0db7a0b485d48bf6
tqma <tqma@trip.com>
2020-06-19 15:14:48 +0800

    remove useless

--- example/index.ts
+++
@@ -1,9 +0,0 @@
-import MFS from 'memory-fs'
-import makeMemoryRequireFunction from '../src/index'
-
-
-const mfs = new MFS()
-const memoryRequire = makeMemoryRequireFunction(mfs)
-mfs.mkdirpSync('/aaa/bbb/')
-mfs.writeFileSync('/aaa/bbb/cc.js', 'module.exports = { test: "aaa" }')
-const module = memoryRequire('/aaa/bbb/cc.js')
