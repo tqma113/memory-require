482cf3bd3b6a849ac3c8b84905e0969a4572f27b
tqma <tqma@trip.com>
2020-06-19 15:18:04 +0800

    update(ci)

--- test/mr.test.ts
+++ test/mr.test.ts
@@ -2,12 +2,36 @@ import MFS from 'memory-fs'
 import makeMemoryRequireFunction from '../src/index'
 
 describe("memory require", () => {
-  it("word", () => {
-    const mfs = new MFS()
-    const memoryRequire = makeMemoryRequireFunction(mfs)
-    mfs.mkdirpSync('/aaa/bbb/')
-    mfs.writeFileSync('/aaa/bbb/cc.js', 'module.exports = { test: "aaa" }')
-    const module = memoryRequire('/aaa/bbb/cc.js')
-    expect(module.exports.test).toEqual('aaa')
+  describe('require', () => {
+    it("work", () => {
+      const mfs = new MFS()
+      const memoryRequire = makeMemoryRequireFunction(mfs)
+
+      mfs.mkdirpSync('/aaa/bbb/')
+      mfs.writeFileSync('/aaa/bbb/cc.js', 'module.exports = { test: "aaa" }')
+      const module = memoryRequire('/aaa/bbb/cc.js')
+      
+      expect(module.exports.test).toEqual('aaa')
+    })
+  })
+
+  describe('require.cache', () => {
+
+  })
+
+  describe('require.extensions', () => {
+
+  })
+
+  describe('require.main', () => {
+
+  })
+
+  describe('require.resolve', () => {
+
+  })
+
+  describe('require.resolve.paths', () => {
+
   })
 })
